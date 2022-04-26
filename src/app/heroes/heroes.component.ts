4cb0e118208f82f7eeb13ac6cd943c6a059ba9b5
Yusuke Kubota <yusuke.kubota@gmo.media>
2022-04-26 16:45:40 +0900

    とりあえず一覧や

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero";
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +8,10 @@ import {Hero} from "../hero";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
@@ -17,4 +22,7 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
